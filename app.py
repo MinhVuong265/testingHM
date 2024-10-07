a34eaf6224f0c5e46b0005c3bf2b1d6d071b2f34
Minh Vương <tmvuong2k4@gmail.com>
2024-10-07 19:24:45 +0700

    update

--- app.py
+++ app.py
@@ -305,4 +305,5 @@ def predict():
         'bagging': bagging_pred.round(2)
     })
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Lấy cổng từ biến môi trường, mặc định là 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
